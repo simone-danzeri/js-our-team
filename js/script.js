19707825104e1e301793afe5a221924190443bd1
simone-danzeri <s.danzeri@icloud.com>
2024-03-20 13:28:43 +0100

    feat: aggiunto array con oggetti

--- js/script.js
+++ js/script.js
@@ -6,3 +6,46 @@
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+// Mi creo l'array che conterrà i vari oggetti
+// Popolo l'array con i vari oggetti
+const teamMembers = [
+    {
+        firstName: 'Wayne',
+        lastName: 'Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        firstName: 'Angela',
+        lastName: 'Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        firstName: 'Walter',
+        lastName: 'Gordon',
+        role: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        firstName: 'Angela',
+        lastName: 'Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        firstName: 'Scott',
+        lastName: 'Estrada',
+        role: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+    {
+        firstName: 'Barbara',
+        lastName: 'Ramos',
+        role: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+console.log(teamMembers);
